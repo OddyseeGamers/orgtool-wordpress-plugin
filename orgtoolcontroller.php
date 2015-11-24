ea2ca3a159991000af0d85b0cdc64cd74b07565e
Martin Skowronski <skowronski.martin@gmail.com>
2015-11-24 20:09:56 +0100

    WIP api

--- orgtoolcontroller.php
+++ orgtoolcontroller.php
@@ -6,6 +6,8 @@ class JSON_API_Orgtool_Controller {
 	  global $wpdb;
 	  $table_name = $wpdb->prefix . "ot_member";
 	  $results = $wpdb->get_results('SELECT * FROM ' . $table_name);
+	  error_log(">>> res", $results->length);
+	  return $results;
   }
 
 }
