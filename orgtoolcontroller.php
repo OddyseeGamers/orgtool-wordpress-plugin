8728826537055ba5d6c87e01702bbe78b85738e3
Martin Skowronski <skowronski.martin@gmail.com>
2015-12-05 14:00:04 +0100

    fixed units api

--- orgtoolcontroller.php
+++ orgtoolcontroller.php
@@ -19,10 +19,14 @@ class JSON_API_Orgtool_Controller {
 
 
   public function units() {
-          global $wpdb;
-          $table_name = $wpdb->prefix . "ot_unit";
-          $results = $wpdb->get_results('SELECT * FROM ' . $table_name);
-          wp_send_json(array('units' => $results));
+	  global $wpdb;
+	  $table_name = $wpdb->prefix . "ot_unit";
+	  $results = $wpdb->get_results('SELECT * FROM ' . $table_name . ' order by id');
+	  foreach($results as $idx => $unit) {
+		  $unit_ids = $wpdb->get_row( 'SELECT id FROM ' . $table_unit . ' WHERE parent = "' . $unit["id"] . '"');
+		  array_push($unit, array('unit_ids' => $unit_ids);
+	  }
+	  wp_send_json(array('units' => $results));
   }
 }
 
